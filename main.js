b13d1871ceb8d2e5a25828caa8bf3401ca15e4ea
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2017-05-25 14:55:07 +0100

    Wiring Up StackNavigator

--- main.js
+++ main.js
@@ -1,21 +1,37 @@
 import Expo from 'expo';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { TabNavigator } from 'react-navigation';
+import { TabNavigator, StackNavigator } from 'react-navigation';
 
 import AuthScreen from './screens/AuthScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
+import MapScreen from './screens/MapScreen';
+import DeckScreen from './screens/DeckScreen';
+import ReviewScreen from './screens/ReviewScreen';
+import SettingScreen from './screens/SettingScreen';
 
-const Main = TabNavigator({
+const MainNavigator = TabNavigator({
     welcome: { screen: WelcomeScreen },
-    auth: { screen: AuthScreen }
+    auth: { screen: AuthScreen },
+    main: {
+      screen: TabNavigator({
+        map: { screen: MapScreen },
+        deck: { screen: DeckScreen },
+        review: {
+          screen: StackNavigator({
+              review: { screen: ReviewScreen },
+              settings: { screen: SettingScreen }
+          })
+        }
+      })
+    }
 });
 
 class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Main />
+        <MainNavigator />
       </View>
     );
   }
