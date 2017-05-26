cd42dbba02ce09ef1b63984f5a90862d0dc235bc
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2017-05-27 00:18:02 +0100

    Using AsyncStorage

---
+++ actions/auth_action.js
@@ -0,0 +1,11 @@
+import { AsyncStorage } from 'react-native';
+import {
+  FACEBOOK_LOGIN_SUCCESS
+} from './types';
+
+// AsyncStorage.setItems('fb_token', token);
+// AsyncStorage.getItems('fb_token');
+
+export const facebookLogin = () => {
+
+};
