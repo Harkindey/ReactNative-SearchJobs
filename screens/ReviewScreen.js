b7726ce870fb37813c94ae5afb58eac6aff0560a
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2017-05-26 12:55:53 +0100

    Programmatic Navigation

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Button } from 'react-native-elements';
 
 class ReviewScreen extends Component {
-  static navigationOptions= {
+  static navigationOptions = ({navigation}) => ({
       title: 'Review Jobs',
-      headerRight:  <Text>Go Right</Text>
-  }
+      headerRight:  <Button title="Settings" onPress={() => navigation.navigate('settings') } />
+  });
 
   render() {
     return (
