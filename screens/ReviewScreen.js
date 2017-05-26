e9b79898c10bca78555279b6d1672d1c2cfda2a9
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2017-05-26 08:47:08 +0100

    NavigationOption

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 class ReviewScreen extends Component {
+  static navigationOptions= {
+      title: 'Review Jobs',
+      headerRight:  <Text>Go Right</Text>
+  }
+
   render() {
     return (
       <View>
