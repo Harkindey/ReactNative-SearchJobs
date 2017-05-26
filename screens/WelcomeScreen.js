df956b771f67b616578296186427d70dedaaa12e
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2017-05-26 15:01:52 +0100

    The Welcome Screen

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import Slides from '../components/Slides';
+
+const SLIDE_DATA = [
+  { text: 'Welcome To JobApp'},
+  { text: 'Use this to get Jobs'},
+  { text: 'Set your location, then swipe away' }
+];
 
 class WelcomeScreen extends Component {
   render() {
     return (
-      <View>
-        <Text>WelcomeScreen</Text>
-        <Text>WelcomeScreen</Text>
-        <Text>WelcomeScreen</Text>
-        <Text>WelcomeScreen</Text>
-        <Text>WelcomeScreen</Text>
-        <Text>WelcomeScreen</Text>
-        <Text>WelcomeScreen</Text>
-      </View>
+      <Slides data={SLIDE_DATA}/>
     );
   }
 }
