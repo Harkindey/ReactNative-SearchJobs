416dcf4b6011ffbf3f12376c1b16a6c6b0276584
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2017-05-26 23:50:06 +0100

    Setting up Redux

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ import Expo from 'expo';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { TabNavigator, StackNavigator } from 'react-navigation';
+import { Provider } from 'react-redux';
 
+import store from './store';
 import AuthScreen from './screens/AuthScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
 import MapScreen from './screens/MapScreen';
@@ -30,9 +32,11 @@ const MainNavigator = TabNavigator({
 class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <MainNavigator />
-      </View>
+      <Provider store={store}>
+        <View style={styles.container}>
+          <MainNavigator />
+        </View>
+    </Provider>
     );
   }
 }
