195b8227422ebd0918d14f1cf2b5a8b65b3ce346
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2017-05-26 15:03:59 +0100

    The Welcome Screen

---
+++ components/Slides.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class Slides extends Component {
+  render() {
+    return (
+      <View>
+
+      </View>
+    );
+  }
+}
+
+export default Slides;
