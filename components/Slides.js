95128ce359cf11851fe18a419e4cf7c5b770fee5
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2017-05-26 16:27:51 +0100

    styling the Welcome Screen

--- components/Slides.js
+++ components/Slides.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 
 class Slides extends Component {
-  render() {
-    return (
-      <View>
-
+  renderSlides() {
+    return this.props.data.map((slide) => {
+      return (
+      <View key={slide.text} style={styles.slideViewStyle}>
+        <Text style={styles.slideTextStyle}>{slide.text}</Text>
       </View>
     );
+    });
+  }
+  render() {
+    return (
+      <ScrollView
+        horizontal
+        style={{ flex: 1 }}
+      >
+        {this.renderSlides()}
+      </ScrollView>
+    );
+  }
+}
+
+const styles = {
+  slideTextStyle:{
+    fontsize: 30
+  },
+  slideViewStyle:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 }
 
