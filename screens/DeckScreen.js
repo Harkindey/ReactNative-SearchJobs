1493122fe375fba4b9893a4d02749a93dc6a082f
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2017-05-27 16:09:57 +0100

    The Deck Screen

--- screens/DeckScreen.js
+++ screens/DeckScreen.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
 
 class DeckScreen extends Component {
   render() {
     return (
       <View>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
+        
       </View>
     );
   }
 }
+function mapStateToProps({ jobs }) {
+    return{ jobs: jobs.results };
+}
 
-export default DeckScreen;
+export default connect(mapStateToProps)(DeckScreen);
