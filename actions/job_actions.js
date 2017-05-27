c1e17d872d64767dab5ae544f4377b3c617fc197
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2017-05-27 12:45:33 +0100

    Fetching a list of Jobs

--- actions/job_actions.js
+++ actions/job_actions.js
@@ -6,6 +6,7 @@ import {
   FETCH_JOBS
 } from './types'
 
+const JOB_ROOT_URL = 'http://api.indeed.com/ads/apisearch?';
 const JOB_QUERY_PARAMS = {
   publisher: '4022372635962623'.
   format; 'json',
@@ -15,9 +16,18 @@ const JOB_QUERY_PARAMS = {
   q: 'javascript'
 };
 
+const buildJobUrl = (zip) => {
+  const query= qs.stringify({ ...JOB_QUERY_PARAMS, l: zip });
+  return `${JOB_ROOT_URL}${query}`;
+};
+
 export const fetchJobs = (region) => async (dispatch) {
   try {
     let zip = await reverseGeocode(region);
+    const url = buildJobUrl(zip);
+    let { data } = await axios.get(url);
+    dispatch({ type: FETCH_JOBS, payload: data });
+    console.log(data);
   } catch(e) {
     console.log(e);
   }
